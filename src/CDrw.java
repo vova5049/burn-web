de6405f790a192f098615d7ded0d0a30a54427cd
vova5049 <kireev@programist.ru>
2016-08-01 12:11:37 +0300

    create class CDrw

--- src/CDrw.java
+++ src/CDrw.java
@@ -1,5 +1,20 @@
 /**
  * Created by vova5049 on 31.07.2016.
  */
-public class CDrw {
+public class CDrw
+{
+
+
+    private String dev;
+    private Boolean active = true;
+    private String iso = "";
+    private String status = "";
+
+
+
+    CDrw(String dev)
+    {
+        this.dev = dev;
+
+    }
 }
